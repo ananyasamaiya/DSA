7f5e2440a2bf153a68256ea043511a5c22c32a82
ananyasamaiya <ananyasamaiya05@gmail.com>
2025-08-16 12:00:42 +0530

    Time: 1 ms (98.64%) | Memory: 42.1 MB (60.08%) - LeetSync

--- 3396-valid-word/valid-word.java
+++ 3396-valid-word/valid-word.java
@@ -1,9 +1,27 @@
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
+
 class Solution {
     public boolean isValid(String word) {
-        Pattern p = Pattern.compile("^(?=.*[aeiouAEIOU])(?=.*[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z])[a-zA-Z0-9]{3,}$");
-        Matcher m = p.matcher(word);
-        return m.matches();
+        word = word.toLowerCase();
+        boolean isVowel = false;
+        boolean isCon = false;
+        if(word.length() < 3) return false;
+        for(int i=0; i<word.length(); i++){
+            char ch = word.charAt(i);
+            if(ch >='0' && ch<='9'){
+                continue;
+            }
+            else if(ch >= 'a' && ch <= 'z'){
+                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                    isVowel = true;
+                }
+                else{
+                    isCon = true; 
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        return isVowel && isCon;
     }
 }
