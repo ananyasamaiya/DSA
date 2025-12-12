dcf96ed32c69315c4f7ad694acddc1ff0c9282b8
ananyasamaiya <ananyasamaiya05@gmail.com>
2025-12-12 22:25:46 +0530

    Time: 21 ms (28.28%) | Memory: 80.7 MB (53.34%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+       for(int num : nums){
+        set.add(num);
+       }
+
+        return (set.size() == nums.length) ? false : true;
+    }
+}
